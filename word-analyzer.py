09f9364f78220132dab8840f3911eadf0b4c283f
Noah Franks <nfranks@purdue.edu>
2017-07-11 12:32:29 -0500

    Removed all code that wasn't written by me. Also the word reading methods can be set to consider either every or only the unique words in a file or directory tree

--- word-analyzer.py
+++ word-analyzer.py
@@ -1,75 +1,46 @@
 import os
 
-def words_from_file(filepath):
+def words_from_file(filepath, unique):
     words = []
 
     with open(filepath, 'r') as file:
         for line in file:
-            words = list(set(words + line.split()))   # Add only the unique words from each line
+            if unique:
+                words = list(set(words + line.split()))   # Add only the unique words from each line
+            else:
+                words = words + line.split()
 
     return words
 
-def words_from_directory(directorypath, verbose=False):
+def words_from_directory(directorypath, unique):
     allwords = []
 
     for root, dirs, files in os.walk(directorypath):
         for filename in files:
             
             filepath = os.path.join(root, filename)
+            print(filepath)
 
-            if verbose:
-                print(filepath)
-
-            allwords = list(set(allwords + words_from_file(filepath)))   # Add only the unique words from each file
+            filewords = words_from_file(filepath, unique)
+            if unique:
+                allwords = list(set(allwords + filewords))   # Add only the unique words from each file
+            else:
+                allwords = allwords + filewords
 
     return allwords
 
-#print words_from_file('../../Desktop/YouTube/Source/120mins/uploaded/downloaded/VCTK-8000-Fake/newText/p9000/p9000_000.txt')
-print words_from_directory('../../Desktop/YouTube/Source/120mins/uploaded/downloaded/VCTK-8000-Fake/newText/', True)
 
-exit(1)
+# A list of every unique word from the directory tree
+distinctWords = words_from_directory(
+    '../../Desktop/YouTube/Source/120mins/uploaded/downloaded/VCTK-8000-Fake/newText/', True)
 
-file = open('../../Desktop/YouTube/Source/120mins/uploaded/downloaded/VCTK-8000-Fake/newText/p9000/p9000_000.txt', 'r')
-book = file.read()
+# A list of every word from the directory tree
+totalityWords = words_from_directory(
+    '../../Desktop/YouTube/Source/120mins/uploaded/downloaded/VCTK-8000-Fake/newText/', False)
 
-
-
-
-def tokenize():
-    if book is not None:
-        words = book.lower().split()
-        return words
-    else:
-        return None
-        
-
-def map_book(tokens):
-    hash_map = {}
-
-    if tokens is not None:
-        for element in tokens:
-            # Remove Punctuation
-            word = element.replace(",","")
-            word = word.replace(".","")
-
-            # Word Exist?
-            if word in hash_map:
-                hash_map[word] = hash_map[word] + 1
-            else:
-                hash_map[word] = 1
-
-        return hash_map
-    else:
-        return None
-
-
-# Tokenize the Book
-words = tokenize()
-word_list = ['the','life','situations','since','day']
-
-# Create a Hash Map (Dictionary)
-map = map_book(words)
-
-# Show Word Information
-for word in word_list:
-    print('Word: [' + word + '] Frequency: ' + str(map[word]))
+for unique in distinctWords:
+    frequency = 0
+    for word in totalityWords:
+        if word == unique:
+            frequency += 1
+    print unique + "\t" + str(frequency)
