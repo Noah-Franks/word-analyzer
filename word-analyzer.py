600d3d6543caca119d3698174b23013fac0280cc
Noah Franks <nfranks@purdue.edu>
2017-07-12 10:48:37 -0500

    Dictionary rewrite finished and successful. Directories that used to take nearly an hour to analyze now take seconds

--- word-analyzer.py
+++ word-analyzer.py
@@ -1,19 +1,20 @@
 import os
 
 def words_from_file(filepath):
-    words = []
+    words = {}
 
     with open(filepath, 'r') as file:
         for line in file:
-            if unique:
-                words = list(set(words + line.split()))   # Add only the unique words from each line
-            else:
-                words = words + line.split()
+            for word in line.split():
+                if word in words:
+                    words[word] += 1
+                else:
+                    words[word] = 1
 
     return words
 
 def words_from_directory(directorypath):
-    allwords = []
+    allwords = {}
 
     for root, dirs, files in os.walk(directorypath):
         for filename in files:
@@ -21,17 +22,20 @@ def words_from_directory(directorypath):
             filepath = os.path.join(root, filename)
             print(filepath)
 
-            filewords = words_from_file(filepath, unique)
-            if unique:
-                allwords = list(set(allwords + filewords))   # Add only the unique words from each file
-            else:
-                allwords = allwords + filewords
+            filewords = words_from_file(filepath)
+            
+            for key, value in filewords.items():
+                if key in allwords:
+                    allwords[key] += value
+                else:
+                    allwords[key] = value
 
     return allwords
 
 
-wordFrequencies = words_from_file('../../Desktop/YouTube/Source/120mins/uploaded/downloaded/VCTK-8000-Fake/newText/', True)
+wordFrequencies = words_from_directory('../../Desktop/YouTube/Source/120mins/uploaded/downloaded/VCTK-8000-Fake/newText/')
+print(wordFrequencies)
 
-with open('word-frequencies.txt', 'w') as output:
-    for word, frequency in wordFrequencies:
+with open('word-frequencies-dictionary.txt', 'w') as output:
+    for word, frequency in wordFrequencies.items():
         output.write(word + " " + str(frequency) + "\n")
