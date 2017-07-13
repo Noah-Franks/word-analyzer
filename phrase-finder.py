0e492599c727f493c36898020b04861bbac58f8f
Noah Franks <nfranks@purdue.edu>
2017-07-13 13:19:18 -0500

    phrase-finder.py: functions now exist for printing out the data for each word, as well as for finding the frequency for each word by percentage of total words

--- phrase-finder.py
+++ phrase-finder.py
@@ -11,7 +11,7 @@ def load_from_file(filepath):   # loads the output of word-analyzer into memory
 
 			if line.find('\t') >= 0:
 
-				disposition =     line[: line.find(' ')      ]
+				disposition =     line[line.find('\t') + 1 : line.find(' ')      ]
 				frequency   = int(line[  line.find(' ') + 1 :])
 
 				data[word][disposition] = frequency
@@ -22,11 +22,24 @@ def load_from_file(filepath):   # loads the output of word-analyzer into memory
 
 	return data
 
+def print_data(data):
+	for word, disposition_dict in data.items():
+		print(word)
+		for disposition, frequency in disposition_dict.items():
+			print("\t%s %s" % (disposition, frequency))
+
+
+def prune_data_by_percentage(data, limit_percentage):   # shortens the amount of data to work with by eliminating rare words
+
+	total_words = 0   # The total number of words in the data as the sum of frequencies
+
+	for word, disposition_dict in data.items():
+		total_words += disposition_dict['total']
+	
+	for word, disposition_dict in data.items():
+		frequency_percentage = disposition_dict['total'] / total_words
+		print(frequency_percentage)
+
 
 data = load_from_file('word-frequencies.txt')
-
-
-for word, disposition_dict in data.items():
-	print(word)
-	for disposition, frequency in disposition_dict.items():
-		print("\t" + disposition + " " + str(frequency))
+prune_data_by_percentage(data, 1.0)
