43306184e328232712d5711bdc12e18d1c4f64e1
Noah Franks <nfranks@purdue.edu>
2017-07-17 15:20:11 -0500

    agents-who-said.sh: saving the current state of the file, which tries to build a list of all agents and selectively remove those who say a particular word if the 'invert' parameter is set, so that I can attempt to solve it a different way

--- agents-who-said.sh
+++ agents-who-said.sh
@@ -3,9 +3,32 @@ if [[ $1 == '' ]]; then
 	exit 1
 fi
 
+invert=$2
+if [[ $invert == '' ]]; then
+	invert='false'
+else 
+	invert='true'
+fi
+
+search_word=$1
+
+speaker=''
+all_speakers=''
+
+selected=''
 while read line; do
 	if [[ ! $line == *' '* ]]; then
-		echo $line
+		speaker=$line
+		all_speakers="$all_speakers|$line"
+	else
+		if [[ $line == *"$search_word"* ]]; then
+			echo "$speaker $line"
+			selected="$selected|$speaker"
+		fi
 	fi
-
 done < agent-profiles.txt
+
+all_speakers="${//\|/ }"
+
+echo "\t$selected"
+echo "\t$all_speakers"
