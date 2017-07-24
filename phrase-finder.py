9c70eeea08c4422d07f8e171c0d2784874631f07
Noah Franks <nfranks@purdue.edu>
2017-07-24 16:42:36 -0500

    phrase-finder.py: statistical analysis performed to determine which pairings of words are statistically more likely to be paired in a particular order, assuming a normal distribution and excluding pairings that never happen

--- phrase-finder.py
+++ phrase-finder.py
@@ -119,11 +119,46 @@ def analyze_word_agent_percentage_composition(data):
 				elif math.fabs(z_score) > 1.960:
 					print('\t%s%s\tp < %s    \t%%: %s\tz: %s' % (word, ' ' * (12 - len(word)), 0.05, percentage, z_score))
 
-def find_phrases(data):
-	
+def analyze_word_phrase_composition(data):
+
+	phrases = {}
+	frequencies = []
+
+	for word, root_dict in data.items():
+		for last_word in root_dict['phrases']:
+
+			frequency = words[word]['phrases'][last_word]
+			phrase = "%s %s" % (last_word, word)
+
+			phrases[phrase] = frequency
+			frequencies.append(frequency)
+
+	mean = sum(frequencies) / len(frequencies)
+	standard_deviation = stats.pstdev(frequencies, mean)
+
+	for phrase in phrases:
+
+		frequency = phrases[phrase]
+		z_score = (frequency - mean) / standard_deviation
+		if math.fabs(z_score) > 3.819:
+			print('\t%s%s\tp < %s  \t%%: %s\tz: %s' % (phrase, ' ' * (12 - len(phrase)), 0.0001, frequency, z_score))
+		elif math.fabs(z_score) > 3.291:
+			print('\t%s%s\tp < %s   \t%%: %s\tz: %s' % (phrase, ' ' * (12 - len(phrase)), 0.001, frequency, z_score))
+		elif math.fabs(z_score) > 2.576:
+			print('\t%s%s\tp < %s    \t%%: %s\tz: %s' % (phrase, ' ' * (12 - len(phrase)), 0.01, frequency, z_score))
+		elif math.fabs(z_score) > 1.960:
+			print('\t%s%s\tp < %s    \t%%: %s\tz: %s' % (phrase, ' ' * (12 - len(phrase)), 0.05, frequency, z_score))
+
+
+
+
 
 words = load_from_file('word-frequencies.txt')
 meta  = load_from_file('meta.txt')
+
+analyze_word_phrase_composition(words)
+exit(0)
+
 words = prune_data_by_percentage(words, meta, 0.5)   # Remove the lower-frequency words to improve the analysis
 
 for argument in sys.argv[1:]:
